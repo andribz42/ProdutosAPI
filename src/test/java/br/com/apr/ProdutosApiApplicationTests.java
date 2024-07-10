0792a09b93ac56c248d7d1d5b645301183eb7665
andribz42 <andersonpribeiro7@gmail.com>
2024-07-09 21:24:33 -0300

    first commit

---
+++ src/test/java/br/com/apr/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
